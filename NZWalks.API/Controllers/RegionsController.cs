34486b7ac1441df5c1b2c8f5e2f4d4debb3e777f
mddanish867 <mddanish867@gmail.com>
2023-12-08 16:37:42 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        public RegionsController()
+        {
+                
+        }
+    }
+}
