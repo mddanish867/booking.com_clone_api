b6dc81b4437788ff8e9f211562d69ee6e0d744d9
mddanish867 <mddanish867@gmail.com>
2024-02-08 12:14:41 +0530

    Changes in NZwalks

---
+++ NZWalks.UI/Controllers/RegionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+
+namespace NZWalks.UI.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task <IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+            try
+            {
+                // Get All region from Web api
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7151/api/regions");
+                httpResponseMessage.EnsureSuccessStatusCode();
+                var stringResponseBody = await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>();
+                ViewBag.Response = stringResponseBody;
+            }
+            catch (Exception ex)
+            {
+
+                //
+            }
+            return View(response);
+        }
+    }
+}
